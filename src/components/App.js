220e680b88eaa237129dcf3f58f06c24bb374db3
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-04-19 20:08:54 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import MovieCard from "./MovieCard";
 
 function App() {
   const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
+  // const posterURL =
+  //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
   const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
 
   return (
@@ -13,5 +13,13 @@ function App() {
     </div>
   );
 }
-
+// function MovieCa({ title, posterSrc, genres }) {
+//   return (
+//     <div className="movie-card">
+//       <img src={posterSrc} alt={title} />
+//       <h2>{title}</h2>
+//       <small>{genres.join(", ")}</small>
+//     </div>
+//   );
+// }
 export default App;
